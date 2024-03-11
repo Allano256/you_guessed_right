2d4dc896eb4e96ad34c8c58b2c5db244691ff849
allan zizinga <allanzizinga@yahoo.com>
2024-03-11 10:54:45 +0100

    tested the play area zones with different color background,created objects with images in js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let footballImages = {
+      name:'Suarez' ,
+      image:'https://free4kwallpapers.com/uploads/originals/2016/04/28/luis-suarez-uruguayan-footballer-wallpaper.jpg',
+      name: 'Messi',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/lionel-messi-argentine-footballer-wallpaper.jpg',
+      
+      name: 'Hazard',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/19/eden-hazard-belgian-footballer-wallpaper.jpg',
+
+      name: 'Iniesta',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/15/andres-iniesta-spanish-footballer-wallpaper.jpg',
+}
+
+let fottballStadiums = {
+    name: 'wembley',
+    image: 'https://a.cdn-hotels.com/gdcs/production67/d1184/209ba1af-54ee-4550-9fd5-f0e80adc25a3.jpg',
+    name: 'Old trafford',
+    image: 'http://www.sportslook.net/wp-content/uploads/2015/03/SD-OldTrafford-1.jpg',
+    name: 'Allianz Arena',
+    image: 'http://www.interpcan.ca/wp-content/uploads/sites/260/2020/03/b178a0878f3ab1bcdbd4e82fcb1b77ae.jpg',
+
+}
