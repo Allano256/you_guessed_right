293d1b1701c6e08b725772a573f717a756ece128
allan zizinga <allanzizinga@yahoo.com>
2024-03-12 10:23:51 +0100

    created all the functions that we shall need in the game,no logic added yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,78 @@
-let footballImages = {
+let footballImages = [{
       name:'Suarez' ,
-      image:'https://free4kwallpapers.com/uploads/originals/2016/04/28/luis-suarez-uruguayan-footballer-wallpaper.jpg',
-      name: 'Messi',
-      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/lionel-messi-argentine-footballer-wallpaper.jpg',
+      image:'https://free4kwallpapers.com/uploads/originals/2016/04/28/luis-suarez-uruguayan-footballer-wallpaper.jpg',width:'100px',height:'100px'},
       
-      name: 'Hazard',
-      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/19/eden-hazard-belgian-footballer-wallpaper.jpg',
+      { name: 'Messi',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/lionel-messi-argentine-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+     ,
+      { name: 'Hazard',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/19/eden-hazard-belgian-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+     ,
+    { name: 'Iniesta',
+    image: 'https://free4kwallpapers.com/uploads/originals/2016/04/15/andres-iniesta-spanish-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+     ,
+]
 
-      name: 'Iniesta',
-      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/15/andres-iniesta-spanish-footballer-wallpaper.jpg',
+// let fottballStadiums = [
+//      {name: 'wembley',
+//      image: 'https://a.cdn-hotels.com/gdcs/production67/d1184/209ba1af-54ee-4550-9fd5-f0e80adc25a3.jpg',width:'100px',height:'100px' }
+//     ,
+//     { name: 'Old trafford',
+//     image: 'http://www.sportslook.net/wp-content/uploads/2015/03/SD-OldTrafford-1.jpg',width:'100px',height:'100px'}
+//    ,
+//     { name: 'Allianz Arena',
+//     image: 'http://www.interpcan.ca/wp-content/uploads/sites/260/2020/03/b178a0878f3ab1bcdbd4e82fcb1b77ae.jpg',width:'100px',height:'100px'}
+//    ,
+
+// ]
+
+/**
+ * 1.Allow the DOM to load and add event
+ * listeners to the buttons.
+ * 2.When the page loads,a random image to appear.
+ * 
+ */
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementsByTagName('button');
+
+})
+
+/* create a function to start running the game */
+
+function runGame(){
+    /* create random pictures from the two arrays */
+
 }
 
-let fottballStadiums = {
-    name: 'wembley',
-    image: 'https://a.cdn-hotels.com/gdcs/production67/d1184/209ba1af-54ee-4550-9fd5-f0e80adc25a3.jpg',
-    name: 'Old trafford',
-    image: 'http://www.sportslook.net/wp-content/uploads/2015/03/SD-OldTrafford-1.jpg',
-    name: 'Allianz Arena',
-    image: 'http://www.interpcan.ca/wp-content/uploads/sites/260/2020/03/b178a0878f3ab1bcdbd4e82fcb1b77ae.jpg',
+
+
+/* Create a function that checks users response */
+
+function userResponse() {
+
+}
+
+/* Create a function that loops throw the array and returns the corrrect answer*/
+
+function correctAnswer(){
+
+}
+
+/* Create a modal that gives  a hint on answer */
+
+function hintAnswer(){
+
+}
+
+/* Increase score */
+
+function scoreIncrease(){
+
+}
+
+/* Decrease score */
+
+function scoreDecrease(){
 
 }
