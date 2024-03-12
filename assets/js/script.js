7f8b46e9eadbd8ef154e5e5373f5649d3db7f6f8
allan zizinga <allanzizinga@yahoo.com>
2024-03-12 11:25:39 +0100

    created a function to generate random images from our array,checked to see that the event listener works on the buttons when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,56 @@
-let footballImages = [{
-      name:'Suarez' ,
-      image:'https://free4kwallpapers.com/uploads/originals/2016/04/28/luis-suarez-uruguayan-footballer-wallpaper.jpg',width:'100px',height:'100px'},
-      
-      { name: 'Messi',
-      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/lionel-messi-argentine-footballer-wallpaper.jpg',width:'100px',height:'100px'}
-     ,
-      { name: 'Hazard',
-      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/19/eden-hazard-belgian-footballer-wallpaper.jpg',width:'100px',height:'100px'}
-     ,
-    { name: 'Iniesta',
-    image: 'https://free4kwallpapers.com/uploads/originals/2016/04/15/andres-iniesta-spanish-footballer-wallpaper.jpg',width:'100px',height:'100px'}
-     ,
-]
+/** create random pictures from array
+ *Create a function that returns a ranom åpicture from array when the display button is clicked */  
+
+  function randomImages(){
+    let footballImagesArray = [{
+        name:'Suarez' ,
+        image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/luis-suarez-uruguayan-footballer-wallpaper.jpg',width:'100px',height:'100px'},
+        
+        { name: 'Messi',
+        image: 'https://free4kwallpapers.com/uploads/originals/2016/04/28/lionel-messi-argentine-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+       ,
+        { name: 'Hazard',
+        image: 'https://free4kwallpapers.com/uploads/originals/2016/04/19/eden-hazard-belgian-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+       ,
+      { name: 'Iniesta',
+      image: 'https://free4kwallpapers.com/uploads/originals/2016/04/15/andres-iniesta-spanish-footballer-wallpaper.jpg',width:'100px',height:'100px'}
+       ,
+  ];
+
+  //Find the length of array of images
+  let arrayLength = footballImagesArray.length;
+  let newArray = [];
+
+  for(let i = 0; i < arrayLength; i++){
+    newArray[i] = newImage();
+    newArray[i].image = footballImagesArray[i].image;
+    newArray[i].width = footballImagesArray[i].width;
+    newArray[i].height = footballImagesArray[i].height;
+
+  }
+
+  //create a random number for the image to be displayed
+  function getRandomNum(min,max){
+    imgNumber = Math.floor(Math.random() * (min - max + 1))
+
+    return newArray[imgNumber];
+  }
+
+  let newImage = getRandomNum(0, newArray.length - 1);
+
+  //Remove the previous image
+   let images = document.getElementsByTagName('image');
+
+   let I = images.length;
+   for(let p = 0; p < I; p++){
+    images[0].parentNode.removeChild(images[0]);
+   }
+
+
+   //Show new random image
+   document.body.appendChild(newImage);
+  }
+
 
 // let fottballStadiums = [
 //      {name: 'wembley',
@@ -34,14 +73,32 @@ let footballImages = [{
  */
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementsByTagName('button');
+    let buttons = document.getElementsByTagName('button');
+
+    for(let button of buttons){
+        button.addEventListener('click', function(){
+            console.log('We active!');
+            alert('We active buttons!');
+            if(this.getAttribute('data-type') === "submit"){
+               checkAnswer();
+            } else {
+                alert("Oh sorry try again!");
+                /* if the person gives a wrong answer,a new image is generated */
+                randomImages();
+            }
+        })
+    }
+    
+    // document.getElementById('randomImage').addEventListener('click', function(){
+
+    // })
 
 })
 
 /* create a function to start running the game */
 
 function runGame(){
-    /* create random pictures from the two arrays */
+    
 
 }
 
@@ -55,7 +112,7 @@ function userResponse() {
 
 /* Create a function that loops throw the array and returns the corrrect answer*/
 
-function correctAnswer(){
+function checkAnswer(){
 
 }
 
